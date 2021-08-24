64158a2d27e0dd7ef57216c5f839da69da3671f7
tienkim9920 <tienkim9920@gmail.com>
2021-08-24 22:59:53 +0700

    update

--- index.js
+++ index.js
@@ -35,12 +35,75 @@ const User = require("./model/user.model");
 
 app.use('/user', userAPI)
 
+let roomCache = []
+
 io.on('connection', async (socket) => {
     console.log('a user connected', socket.id);
 
+    setInterval(() => {
+        
+        const newCache = roomCache.filter(value => {
+            return parseInt(value.expiredTime) > parseInt(Date.now())
+        })
+
+        roomCache = newCache
+
+    }, 90000)
+
     socket.on('room', (data) => {
-        socket.join(data)
         console.log(`${socket.id} da tham gia phong ${data}`)
+        socket.join(data)
+
+        const cache = {
+            id: socket.id,
+            room: data,
+            expiredTime: Date.now() + 480000
+        }
+
+        roomCache.push(cache)
+    })
+
+    //Nhận socket
+    socket.on('findRoom', (room) => {
+
+        // Đầu tiên phải getRoom trước
+        let getRoom = false
+
+        let player = 0
+
+        roomCache.forEach(value => {
+            if (value.room === room){
+                player++
+                getRoom = true
+            }
+        })
+
+        // set data để gửi lại client
+        const body = {
+            player, room, getRoom
+        }
+
+        // Lưu vào cache
+        const cache = {
+            id: socket.id,
+            room,
+            expiredTime: Date.now() + 480000
+        }
+
+        // Kiểm tra xem thử phòng đó có bao nhiêu người chơi
+        if (player < 2 && getRoom === true){
+            console.log(`${socket.id} da tham gia phong ${room}`)
+            socket.join(room)
+
+            roomCache.push(cache)
+        
+            // Gủi riêng cho chính thg đó :))
+            io.to(`${socket.id}`).emit('findRoom', body)
+        }else{
+            // Gủi riêng cho chính thg đó :))
+            io.to(`${socket.id}`).emit('findRoom', body)
+        }
+
     })
 
     socket.on('position', (data) => {
@@ -67,7 +130,18 @@ io.on('connection', async (socket) => {
         socket.in(room).emit('replay')
     })
 
+    // Nhận socket rời phòng
     socket.on('leaveRoom', (room) => {
+
+        // Tìm trong cache đối tượng đã rời phòng
+        const index = roomCache.findIndex(value => {
+            return value.room.toString() === room.toString() && 
+            value.id.toString() === socket.id.toString()
+        })
+
+        // sau đó xóa nó đi
+        roomCache.splice(index, 1)
+
         socket.in(room).emit('leaveRoom')
     })
 
